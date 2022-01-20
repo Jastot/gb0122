cc5210450d2c712de308f18dfaf753063e20c792
Георгий Чудаков <george1005@yandex.ru>
2022-01-20 17:25:37 +0300

    HW3

--- Assets/Scripts/PhotonLogin.cs
+++ Assets/Scripts/PhotonLogin.cs
@@ -1,8 +1,14 @@
 using Photon.Pun;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PhotonLogin : MonoBehaviourPunCallbacks
 {
+    [SerializeField] private TextMeshProUGUI _textMeshProUGUI;
+    [SerializeField] private Button _button;
+    private bool _buttonCondition;
+    private TextMeshProUGUI _buttonText;
     private void Awake()
     {
         PhotonNetwork.AutomaticallySyncScene = true;
@@ -10,10 +16,20 @@ public class PhotonLogin : MonoBehaviourPunCallbacks
 
     private void Start()
     {
-        Connect();
+        _buttonCondition = true;
+        _textMeshProUGUI.color = Color.magenta;
+        _buttonText = _button.GetComponentInChildren<TextMeshProUGUI>();
     }
 
-    public void Connect()
+    public void ButtonShifter()
+    {
+        if (_buttonCondition)
+            Connect();
+        else
+            Disconnect();
+    }
+    
+    private void Connect()
     {
         if (PhotonNetwork.IsConnected)
         {
@@ -23,8 +39,32 @@ public class PhotonLogin : MonoBehaviourPunCallbacks
         {
             PhotonNetwork.ConnectUsingSettings();
         }
+        SetConditionAndShowInfo();
     }
 
+    private void Disconnect()
+    {
+        if (PhotonNetwork.IsConnected)
+            PhotonNetwork.Disconnect();
+        SetConditionAndShowInfo();
+    }
+
+    private void SetConditionAndShowInfo()
+    {
+        if (_buttonCondition)
+        {
+            _textMeshProUGUI.text = "Connect";
+            _buttonText.text = "Log out";
+            _buttonCondition = false;
+        }
+        else
+        {
+            _textMeshProUGUI.text = "Disconnect";
+            _buttonText.text = "Log in";
+            _buttonCondition = true;
+        }
+    }
+    
     public override void OnConnectedToMaster()
     {
         base.OnConnectedToMaster();
